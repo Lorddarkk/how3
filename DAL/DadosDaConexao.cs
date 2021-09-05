f8759dc9e050de042254a524b9f7442da0f5c1ff
Lorddarkk <81494970+Lorddarkk@users.noreply.github.com>
2021-09-05 13:59:19 -0300

    Add files via upload

---
+++ DAL/DadosDaConexao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexao
+    {
+        public static string servidor = @"VM-ESTUDO";
+        public static string banco = "ControleDeEstoque";
+        public static string usuario = "sa";
+        public static string senha = "bill+1212";
+        public static String StringDeConexao
+        {
+            get 
+            {
+                return @"Data Source="+servidor+";Initial Catalog="+banco+";User ID="+usuario+";Password="+senha;
+            }
+        }
+    }
+}
